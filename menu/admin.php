57be1e22147edcb3d17218c6b4d4afea7c4a815c
Adikasa <adidayakreasidigital@gmail.com>
2025-07-27 01:10:53 +0700

    first commit

---
+++ menu/admin.php
@@ -0,0 +1,53 @@
+<div class="menu_section">
+  <h3>Umum</h3>
+    <ul class="nav side-menu">
+      <li><a href="main.php?module=home_admin"><i class="fa fa-home"></i> Beranda </a>
+      </li>
+      <li><a><i class="fa fa-sitemap"></i> Master Data <span class="fa fa-chevron-down"></span></a>
+          <ul class="nav child_menu">
+            
+            <li><a><i class="fa fa-warning"></i> Pelanggaran <span class="fa fa-chevron-down"></span></a>
+              <ul class="nav child_menu">
+                <li><a href="main.php?module=pelanggaran">Pelanggaran</a></li>
+                <li><a href="main.php?module=tindakan">Tindakan</a></li>
+              </ul>
+            </li>
+            <li><a href="main.php?module=prestasi"><i class="fa fa-star-o"></i> Prestasi</a>
+            </li>
+            <li><a href="main.php?module=alumni"><i class="fa fa-users"></i> Alumni </a>
+            </li>
+            <li><a href="main.php?module=siswa_keluar"><i class="fa fa-users"></i> Siswa Keluar / Pindah </a>
+            </li> 
+            <li><a href="main.php?module=jurusan"><i class="fa fa-navicon"></i> Prog. Keahlian </a>
+            </li>
+            <li><a href="main.php?module=kelas"><i class="fa fa-navicon"></i> Kelas </a>
+            </li> 
+             <li><a><i class="fa fa-user"></i> User <span class="fa fa-chevron-down"></span></a>
+              <ul class="nav child_menu">
+                <li><a href="main.php?module=guru"><i class="fa fa-graduation-cap"></i> Guru </a></li>
+                <li><a href="main.php?module=ortu"><i class="fa fa-male"></i> Orang Tua </a></li>
+                <li><a href="main.php?module=siswa"><i class="fa fa-users"></i> Siswa </a></li>
+                <li><a href="main.php?module=user"><i class="fa fa-key"></i> Akun User </a></li>
+              </ul>
+            </li>
+          </ul>
+      </li>
+    </ul>
+</div>
+      
+<div class="menu_section">
+  <h3>Pembinaan Karakter</h3>
+    <ul class="nav side-menu">
+      <li><a href="main.php?module=input_pelanggaran_siswa"><i class="fa fa-pencil-square-o"></i>Pelanggaran Siswa </a>
+      </li>
+      <li><a href="main.php?module=input_prestasi_siswa"><i class="fa fa-pencil-square-o"></i>Prestasi Siswa </a>
+      </li>
+      <li><a><i class="fa fa-newspaper-o"></i> Laporan <span class="fa fa-chevron-down"></span></a>
+        <ul class="nav child_menu">
+          <li><a href="main.php?module=laporan_pelanggaran">Lap.Pelanggaran</a></li>
+          <li><a href="main.php?module=laporan_prestasi">Lap. Prestasi</a></li>
+        </ul>
+      </li>
+      <li><a href="logout.php"><i class="fa fa-sign-out"></i> Keluar</a></li>
+    </ul>
+</div>
